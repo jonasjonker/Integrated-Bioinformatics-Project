f72b2af72b4178ee6f502faccf3740295d8e829a
Kristen Michelle Nader <61286548+kris-nader@users.noreply.github.com>
2020-10-26 00:07:18 +0100

    Update create_seurat.R
    A functional create_seurat script

--- create_seurat.R
+++ create_seurat.R
@@ -2,8 +2,8 @@ library(Seurat)
 library(data.table)
 library(purrr)
 
-basedir <- "/Users/ricard/data/10x_rna_atac/original"
-outfile <- "/Users/ricard/data/10x_rna_atac/seurat.rds"
+#basedir <- "/Users/ricard/data/10x_rna_atac/original"
+#outfile <- "/Users/ricard/data/10x_rna_atac/seurat.rds"
 
 
 ###############
@@ -11,7 +11,8 @@ outfile <- "/Users/ricard/data/10x_rna_atac/seurat.rds"
 ###############
 
 # counts <- Read10X_h5(paste0(basedir,"/filtered_feature_bc_matrix.h5"))
-counts <- Read10X(paste0(basedir,"/filtered_feature_bc_matrix"))
+counts <- Read10X(paste0("C:\\Users\\user\\Desktop\\pbmc_unsorted_3k_filtered_feature_bc_matrix\\","filtered_feature_bc_matrix"))
+
 
 ###################
 ## Create Seurat ##
@@ -32,39 +33,32 @@ seurat
 ## Add metadata ##
 ##################
 
-metadata <- fread("/Users/ricard/data/10x_rna_atac/sample_metadata.csv") %>%
-  .[,barcode:=gsub("-1","",barcode)]
-
+#metadata <- fread("C:\\Users\\user\\Desktop\\sample_metadata.csv") %>% .[,barcode:=gsub("-1","",barcode)]
+metadata <- fread("C:\\Users\\user\\Desktop\\metadata_file_3k_unsorted.csv")
 rename_celltypes <- c(
-  "naive CD4 T cells" = "Lymphoid",
-  "memory CD4 T cells" = "Lymphoid",
-  "naive CD8 T cells" = "Lymphoid",
-  "CD56 \\(bright\\) NK cells" = "Lymphoid",
-  "CD56 \\(dim\\) NK cells" = "Lymphoid",
-  "memory B cells" = "Lymphoid",
-  "naive B cells" = "Lymphoid",
-  "effector CD8 T cells" = "Lymphoid",
-  "MAIT T cells" = "Lymphoid",
-  "non-classical monocytes" = "Myeloid",
-  "intermediate monocytes" = "Myeloid",
-  "classical monocytes" = "Myeloid",
-  "myeloid DC" = "Myeloid",
-  "plasmacytoid DC" = "Lymphoid"
+  "B cell progenitor"="Lymphoid",
+  "CD4 Memory"="Lymphoid",
+  "CD8 Naive"="Lymphoid",
+  "NK cell"="Lymphoid",
+  "CD14 Monocytes"= "Myeloid",
+  "CD4 Naive"="Lymphoid",
+  "Dendritic cell"="Lymphoid",
+  "pDC"="Lymphoid",
+  "CD16 Monocytes"="Myeloid",
+  "CD8 effector"="Lymphoid",
+  "Double negative T cell"="Lymphoid",
+  "pre-B cell"="Lymphoid"
 )
 metadata$broad_celltype <- stringr::str_replace_all(metadata$celltype,rename_celltypes)
 
 dt <- data.table(barcode=colnames(seurat)) %>%
-  merge(metadata,by="barcode", all.x=TRUE) %>%
+  merge(metadata,by="barcode",all.x= TRUE) %>%
   .[,c("pass_rnaQC","pass_accQC"):=FALSE] %>%
   .[!is.na(celltype),c("pass_rnaQC","pass_accQC"):=TRUE] %>%
   tibble::column_to_rownames("barcode")
 
-seurat <- AddMetaData(seurat, dt)
+
+seurat <- AddMetaData(seurat,dt)
 
 head(seurat@meta.data)
 
-##########
-## Save ##
-##########
-
-saveRDS(seurat, outfile, compress = FALSE)
