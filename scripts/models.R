6f47565a4e1704468d8338a95a0c50401313eca3
James O'Reilly <jamesdanieloreilly@gmail.com>
2020-12-07 00:46:40 +0100

    Nested single-gene analyses in for loops for statistical robustness

--- scripts/models.R
+++ scripts/models.R
@@ -130,7 +130,7 @@ singleGeneCorrelation <- function(geneName) {
 }
 
 # fit a linear regression to data from a single gene
-singleGeneRegression <- function(geneName) {
+singleGeneRegression <- function(geneName, plot=FALSE) {
   data = getGeneData(geneName)
   split_data = splitData(data, prop=0.8)
   train = split_data[[1]]
@@ -142,13 +142,15 @@ singleGeneRegression <- function(geneName) {
   conf.int <-predict(model, interval="confidence")
   lmdata <- cbind(train, conf.int)
   
-  p <- ggplot(lmdata, aes(GSM, GEX)) +
-       ggtitle(paste(geneName, "Linear Regression")) +
-       geom_point() +
-       stat_smooth(method=lm) +
-       geom_line(aes(y=lwr), color="red", linetype="dashed") +
-       geom_line(aes(y=upr), color="red", linetype="dashed")
-  print(p)
+  if (plot==TRUE) {
+    p <- ggplot(lmdata, aes(GSM, GEX)) +
+      ggtitle(paste(geneName, "Linear Regression")) +
+      geom_point() +
+      stat_smooth(method=lm) +
+      geom_line(aes(y=lwr), color="red", linetype="dashed") +
+      geom_line(aes(y=upr), color="red", linetype="dashed")
+    print(p) 
+  }
   
   eval_data = data.frame(expected=test$GEX, predicted=preds)
   correlation = cor(eval_data[,1], eval_data[,2])
@@ -156,20 +158,26 @@ singleGeneRegression <- function(geneName) {
 }
 
 # fit a smoothing spline to data from a single gene
-singleGeneSpline <- function(geneName, cv=FALSE, df=5) {
+singleGeneSpline <- function(geneName, cv=TRUE, df=5, plot=FALSE) {
   data = getGeneData(geneName)
   split_data = splitData(data, prop=0.8)
   train = split_data[[1]]
   test = split_data[[2]]
   
-  plot(train$GSM, train$GEX, cex=.5, main=paste(geneName, " Smoothing Spline"))
+  if (plot==TRUE) {
+    plot(train$GSM, train$GEX, cex=.5, main=paste(geneName, " Smoothing Spline")) 
+  }
+  
   if (cv==TRUE) {
     fit = smooth.spline(train$GSM, train$GEX, cv=TRUE)
-  }
-  else {
+  }  else {
     fit = smooth.spline(train$GSM, train$GEX, df=df)
   }
-  lines(fit, col="red", lwd=2)
+  
+  if (plot==TRUE) {
+    lines(fit, col="red", lwd=2)
+  }
+
   pred = predict(fit, test$GSM, se=T)
   eval_data = data.frame(expected=test$GEX, predicted=pred[2])
   correlation = cor(eval_data[,1], eval_data[,2])
@@ -274,7 +282,7 @@ NAMPT_cor <- singleGeneCorrelation("NAMPT")
 myeloid_cor <- c(LYN_cor, SLC8A1_cor, PLXDC2_cor, LRMDA_cor, NAMPT_cor)
 
 # BROAD CELL-TYPE
-broad_cell_type_cor <- c(lymphoid_cor, myeloid_cor)
+cor_bct <- c(lymphoid_cor, myeloid_cor)
 
 # Non-specific
 NEAT1_cor <- singleGeneCorrelation("NEAT1")
@@ -282,143 +290,72 @@ ZEB2_cor <- singleGeneCorrelation("ZEB2")
 IL12RB1_cor <- singleGeneCorrelation("IL12RB1")
 PLCB1_cor <- singleGeneCorrelation("PLCB1")
 SERINC5_cor <- singleGeneCorrelation("SERINC5")
-non_specific_cor <- c(NEAT1_cor, ZEB2_cor, IL12RB1_cor, PLCB1_cor, SERINC5_cor)
-
-# plotting specific vs non-specific
-names_cor = c("broad cell-type", "non-specific")
-barplot(c(mean(broad_cell_type_cor), mean(non_specific_cor)), names=names_cor, col='darkred', ylab="correlation", width=c(0.1,0.1), space=c(0.2,0.2))
+cor_ns <- c(NEAT1_cor, ZEB2_cor, IL12RB1_cor, PLCB1_cor, SERINC5_cor)
 
 ###### SINGLE GENE REGRESSION ######
 
-# LYMPHOID
-BCL11B <- singleGeneRegression("BCL11B")
-CD8A <- singleGeneRegression("CD8A")
-CCR7 <- singleGeneRegression("CCR7")
-lymphoid <- c(BCL11B, CD8A, CCR7)
+# initialise empty vectors to store results
+reg_bct <- c()
+reg_ns <- c()
 
-# MYELOID
-LYN <- singleGeneRegression("LYN")
-SLC8A1 <- singleGeneRegression("SLC8A1")
-PLXDC2 <- singleGeneRegression("PLXDC2")
-LRMDA <- singleGeneRegression("LRMDA")
-NAMPT <- singleGeneRegression("NAMPT")
-myeloid <- c(LYN, SLC8A1, PLXDC2, LRMDA, NAMPT)
-
-# BROAD CELL-TYPE
-broad_cell_type <- c(lymphoid, myeloid)
-
-# B-Cell
-# BANK1 <- singleGeneRegression("BANK1")
-# BCL11B <- singleGeneRegression("BCL11B")
-# BLK <- singleGeneRegression("BLK")
-# CD79A <- singleGeneRegression("CD79A")
-# bcell <- c(BANK1, BCL11B, BLK, CD79A)
-# 
-# # DENDRITIC
-# PLD4 <- singleGeneRegression("PLD4")
-# GZMB <- singleGeneRegression("GZMB")
-# dendritic <- c(PLD4, GZMB)
-# 
-# # GRANULOCYTES
-# CCR3 <- singleGeneRegression("CCR3")
-# MS4A3 <- singleGeneRegression("MS4A3")
-# FCGR3B <- singleGeneRegression("FCGR3B")
-# granulocytes <- c(CCR3, MS4A3, FCGR3B)
-# 
-# # NATURAL KILLER
-# KLRF1 <- singleGeneRegression("KLRF1")
-# XCL2 <- singleGeneRegression("XCL2")
-# natural_killer <- c(KLRF1, XCL2)
-# 
-# # T-CELLS
-# CCR7 <- singleGeneRegression("CCR7")
-# CD8B <- singleGeneRegression("CD8B")
-# CCL5 <- singleGeneRegression("CCL5")
-# t_cells <- c(CCR7, CD8B, CCL5)
-
-# Non-specific
-NEAT1 <- singleGeneRegression("NEAT1")
-ZEB2 <- singleGeneRegression("ZEB2")
-IL12RB1 <- singleGeneRegression("IL12RB1")
-PLCB1 <- singleGeneRegression("PLCB1")
-SERINC5 <- singleGeneRegression("SERINC5")
-non_specific <- c(NEAT1, ZEB2, IL12RB1, PLCB1, SERINC5)
-
-# plotting specific vs non-specific
-names = c("broad cell-type", "non-specific")
-barplot(c(mean(broad_cell_type), mean(non_specific)), names=names, col='darkred', ylab="correlation", width=c(0.1,0.1), space=c(0.2,0.2))
-
-# lymphoid, myeloid, broad cell-type, and non-specific
-names = c("lymphoid", "myeloid", "broad cell-type", "non-specific")
-barplot(c(mean(lymphoid), mean(myeloid), mean(broad_cell_type), mean(non_specific)), names=names)
+for (i in 1:10) {
+  # Broad cell-type
+  BCL11B <- singleGeneRegression("BCL11B")
+  CD8A <- singleGeneRegression("CD8A")
+  CCR7 <- singleGeneRegression("CCR7")
+  LYN <- singleGeneRegression("LYN")
+  SLC8A1 <- singleGeneRegression("SLC8A1")
+  PLXDC2 <- singleGeneRegression("PLXDC2")
+  LRMDA <- singleGeneRegression("LRMDA")
+  NAMPT <- singleGeneRegression("NAMPT")
+  bct <- mean(c(BCL11B, CD8A, CCR7, LYN, SLC8A1, PLXDC2, LRMDA, NAMPT))
+  reg_bct <- c(reg_bct, bct)
+  
+  # Non-specific
+  NEAT1 <- singleGeneRegression("NEAT1")
+  ZEB2 <- singleGeneRegression("ZEB2")
+  IL12RB1 <- singleGeneRegression("IL12RB1")
+  PLCB1 <- singleGeneRegression("PLCB1")
+  SERINC5 <- singleGeneRegression("SERINC5")
+  ns <- mean(c(NEAT1, ZEB2, IL12RB1, PLCB1, SERINC5))
+  reg_ns <- c(reg_ns, ns) 
+}
 
 ###### SINGLE GENE SPLINE ######
-# LYMPHOID
-BCL11B_spline <- singleGeneSpline("BCL11B")
-CD8A_spline <- singleGeneSpline("CD8A")
-CCR7_spline <- singleGeneSpline("CCR7")
-lymphoid_spline <- c(BCL11B_spline, CD8A_spline, CCR7_spline)
 
-# MYELOID
-LYN_spline <- singleGeneSpline("LYN")
-SLC8A1_spline <- singleGeneSpline("SLC8A1")
-PLXDC2_spline <- singleGeneSpline("PLXDC2")
-LRMDA_spline <- singleGeneSpline("LRMDA")
-NAMPT_spline <- singleGeneSpline("NAMPT")
-myeloid_spline <- c(LYN_spline, SLC8A1_spline, PLXDC2_spline, LRMDA_spline, NAMPT_spline)
+# initialise empty vectors to store results
+spl_bct <- c()
+spl_ns <- c()
 
-# BROAD CELL-TYPE
-broad_cell_type_spline <- c(lymphoid_spline, myeloid_spline)
+for (i in 1:10) {
 
-# # B-Cell
-# BANK1_spline <- singleGeneSpline("BANK1")
-# BCL11B_spline <- singleGeneSpline("BCL11B")
-# BLK_spline <- singleGeneSpline("BLK")
-# CD79A_spline <- singleGeneSpline("CD79A")
-# bcell_spline <- c(BANK1_spline, BCL11B_spline, BLK_spline, CD79A_spline)
-# 
-# # DENDRITIC
-# # PLD4_spline <- singleGeneSpline("PLD4")
-# GZMB_spline <- singleGeneSpline("GZMB")
-# dendritic_spline <- c(GZMB_spline)
-# 
-# # GRANULOCYTES
-# CCR3_spline <- singleGeneSpline("CCR3")
-# MS4A3_spline <- singleGeneSpline("MS4A3")
-# FCGR3B_spline <- singleGeneSpline("FCGR3B")
-# granulocytes_spline <- c(CCR3_spline, MS4A3_spline, FCGR3B_spline)
-# 
-# # NATURAL KILLER
-# KLRF1_spline <- singleGeneSpline("KLRF1")
-# XCL2_spline <- singleGeneSpline("XCL2")
-# natural_killer_spline <- c(KLRF1_spline, XCL2_spline)
-# 
-# # T-CELLS
-# CCR7_spline <- singleGeneSpline("CCR7")
-# CD8B_spline <- singleGeneSpline("CD8B")
-# CCL5_spline <- singleGeneSpline("CCL5")
-# t_cells_spline <- c(CCR7_spline, CD8B_spline, CCL5_spline)
-
-# Non-specific
-NEAT1_spline <- singleGeneSpline("NEAT1")
-ZEB2_spline <- singleGeneSpline("ZEB2")
-IL12RB1_spline <- singleGeneSpline("IL12RB1")
-PLCB1_spline <- singleGeneSpline("PLCB1")
-SERINC5_spline <- singleGeneSpline("SERINC5")
-non_specific_spline <- c(NEAT1_spline, ZEB2_spline, IL12RB1_spline, PLCB1_spline, SERINC5_spline)
-
-# plotting specific vs non-specific
-names = c("broad cell-type", "non-specific")
-barplot(c(mean(broad_cell_type_spline), mean(non_specific_spline)), names=names, col='darkred', ylab="correlation")
-
-# lymphoid, myeloid, broad cell-type, and non-specific
-names = c("lymphoid", "myeloid", "broad cell-type", "non-specific")
-barplot(c(mean(lymphoid_spline), mean(myeloid_spline), mean(broad_cell_type_spline), mean(non_specific_spline)), names=names)
+  # LYMPHOID
+  BCL11B_spline <- singleGeneSpline("BCL11B")
+  CD8A_spline <- singleGeneSpline("CD8A")
+  CCR7_spline <- singleGeneSpline("CCR7")
+  LYN_spline <- singleGeneSpline("LYN")
+  SLC8A1_spline <- singleGeneSpline("SLC8A1")
+  PLXDC2_spline <- singleGeneSpline("PLXDC2")
+  LRMDA_spline <- singleGeneSpline("LRMDA")
+  NAMPT_spline <- singleGeneSpline("NAMPT")
+  bct <- mean(c(BCL11B_spline, CD8A_spline, CCR7_spline, LYN_spline, SLC8A1_spline, PLXDC2_spline, LRMDA_spline, NAMPT_spline))
+  spl_bct <- c(spl_bct, bct)
+ 
+  # Non-specific
+  NEAT1_spline <- singleGeneSpline("NEAT1")
+  ZEB2_spline <- singleGeneSpline("ZEB2")
+  IL12RB1_spline <- singleGeneSpline("IL12RB1")
+  PLCB1_spline <- singleGeneSpline("PLCB1")
+  SERINC5_spline <- singleGeneSpline("SERINC5")
+  ns <- mean(c(NEAT1_spline, ZEB2_spline, IL12RB1_spline, PLCB1_spline, SERINC5_spline))
+  spl_ns <- c(spl_ns, ns)
+}
 
 ###### SINGLE GENE MODEL COMPARISONS ######
 # plotting broad cell vs non-specific for correlation, linear, splines
-names = c("broad cell-type", "non-specific","broad cell-type_spline", "non-specific_spline")
-barplot(c(mean(broad_cell_type), mean(non_specific), mean(broad_cell_type_spline), mean(non_specific_spline)), names=names, col='darkred', ylab="correlation")
+names = c("Broad", "NS", "broad linear", "NS linear","broad spline", "NS spline")
+barplot(c(mean(cor_bct), mean(cor_ns), mean(reg_bct), mean(reg_ns), mean(spl_bct), mean(spl_ns)), names=names, col='darkred', ylab="correlation")
+
 
 ######## GENE ONTOLOGY #######
 # running some sample gene ontology models to show poor performance
